e74edb31d677322d8a748ed4179d7a64896f6a80
Yash Raj Sinha <yrs2@uw.edu>
2020-01-15 14:26:56 -0800

    first problem done

---
+++ HW1/hw1.R
@@ -0,0 +1,14 @@
+# Problem 2.7
+a1 <- pt(-2.31, df = 18)
+print(paste("p-val for t0 = 2.31: ", a))
+
+b1 <- pt(-3.60, df = 18)
+print(paste("p-val for t0 = 2.31: ", a))
+
+c1 <- pt(-1.95, df = 18)
+print(paste("p-val for t0 = 2.31: ", a))
+
+d1 <- pt(-2.19, df = 18)
+print(paste("p-val for t0 = 2.31: ", a))
+
+
